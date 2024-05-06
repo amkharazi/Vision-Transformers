11c2f454572386e7653c3c8aea068df0bfb6a64a
amkharazi <a.m.kharazi.ac@gmail.com>
2024-05-06 11:21:42 +0330

    updated-server

--- Models/tensorized_components/patch_embedding.py
+++ Models/tensorized_components/patch_embedding.py
@@ -1,24 +1,29 @@
+
+import sys
+sys.path.append('..')
 import torch
 import torch.nn as nn
 from einops import rearrange
 
+from Tensorized_Layers.TCL import TCL
+# from Tensorized_Layers.TRL import TRL
+
 class PatchEmbedding(nn.Module):
-    def __init__(self, patch_size, in_channels, embed_dim):
+    def __init__(self,input_size, patch_size, in_channels, embed_dim, device):
         super(PatchEmbedding, self).__init__()
         self.patch_size = patch_size
-        self.embed_dims = embed_dim
-        self.in_channels = in_channels
-
-        self.w_h = nn.Parameter(torch.randn(self.patch_size, self.embed_dims[0]), requires_grad=True)
-        self.w_w = nn.Parameter(torch.randn(self.patch_size, self.embed_dims[1]), requires_grad=True)
-        self.w_c = nn.Parameter(torch.randn(self.in_channels, self.embed_dims[2]), requires_grad=True)
+        self.tcl_input_size =  (input_size[0], input_size[1], input_size[2],
+                                patch_size, patch_size, in_channels) # patched input image size
+                                
+        self.tcl = TCL(input_size=self.tcl_input_size,
+                        rank=embed_dim,
+                        ignore_modes=(0,1,2),
+                        bias=True, 
+                        device=device)
 
     def forward(self, x):
         x = rearrange(x, 
                         'b c (p1 h) (p2 w) -> b p1 p2 h w c',
                         h=self.patch_size, w=self.patch_size) # X = [B P1 P2 H W C]
-        x = torch.einsum(
-                        'b p q h w c , h x , w y , c z  -> b p q x y z',
-                        (x, self.w_h, self.w_w, self.w_c)
-                        ) # X = [B P1 P2 D1 D2 D3]
+        x = self.tcl(x) # X = [B P1 P2 D1 D2 D3]
         return x # patches
