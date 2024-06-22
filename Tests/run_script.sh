3c5ff82770d00fc86d8e1e0f9f350fcdd075f3ac
amkharazi <a.m.kharazi.ac@gmail.com>
2024-06-22 12:22:59 +0330

    updated

--- Tests/run_script.sh
+++ Tests/run_script.sh
@@ -6,9 +6,9 @@
 # Uncomment test cases you wish to run, then run the bash file
 
 # python3 TEST_001.py
-python3 TEST_001_val.py	
+# python3 TEST_001_val.py	
 # python3 TEST_002.py	
-python3 TEST_002_val.py
+# python3 TEST_002_val.py
 # python3 TEST_003.py
 # python3 TEST_004.py
 # python3 TEST_005.py
@@ -20,9 +20,9 @@ python3 TEST_002_val.py
 # python3 TEST_011.py
 # python3 TEST_012.py
 # python3 TEST_013.py
-python3 TEST_013_val.py	
+# python3 TEST_013_val.py	
 # python3 TEST_014.py
-python3 TEST_014_val.py	
+# python3 TEST_014_val.py	
 # python3 TEST_015.py
 # python3 TEST_016.py
 # python3 TEST_017.py
@@ -34,14 +34,14 @@ python3 TEST_014_val.py
 # python3 TEST_023.py
 # python3 TEST_024.py
 # python3 TEST_025.py
-python3 TEST_025_val.py
+# python3 TEST_025_val.py
 # python3 TEST_026.py
 # python3 TEST_027.py
 # python3 TEST_028.py
 # python3 TEST_029.py
 # python3 TEST_030.py
 # python3 TEST_031.py
-python3 TEST_031_val.py
+# python3 TEST_031_val.py
 # python3 TEST_032.py
 # python3 TEST_033.py
 # python3 TEST_034.py
